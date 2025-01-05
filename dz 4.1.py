d4171b02aa654a9e7b03f6a9bb5376a8fde22a0b
Sokolov53421 <isokolov55530@gmail.com>
2025-01-05 03:51:44 +0200

    dz 3.1

---
+++ dz 4.1.py
@@ -0,0 +1,8 @@
+my_list = [0,9,0,8,0,6,9,0,9,8,9,0]
+number_zero = 0
+
+zero = my_list.count(number_zero)
+my_list = [i for i in my_list if i != number_zero]
+my_list.extend([number_zero] * zero)
+
+print(my_list)
